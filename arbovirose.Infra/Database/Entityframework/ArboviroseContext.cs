454bdc079622d2eee3d7e2ee99b0250a68a5ad31
Gabriel <devzando8182@gmail.com>
2024-05-29 20:10:39 -0300

    adicionando conexão com banco de dados

---
+++ arbovirose.Infra/Database/Entityframework/ArboviroseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace arbovirose.Infra.Database.Entityframework
+{
+    public class ArboviroseContext : DbContext
+    {
+        public ArboviroseContext(DbContextOptions<ArboviroseContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ArboviroseContext).Assembly);
+        }
+
+    }
+}
