de1bd13bc7f32ee085d3d1f95662effb7e888666
Gabriel <devzando8182@gmail.com>
2024-05-29 21:07:11 -0300

    Feat: adicionando entidade de usuário

---
+++ arbovirose.Domain/Entities/UserEntity.cs
@@ -0,0 +1,27 @@
+﻿using arbovirose.Domain.ValueObjects;
+
+namespace arbovirose.Domain.Entities
+{
+    public class UserEntity
+    {
+        public UserEntity () { }
+        public UserEntity (
+            string Name,
+            Email Email,
+            string Password
+        ) 
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = Name;
+            this.Email = Email;
+            this.Password = Password;
+            this.UniqueCode = new UniqueCode();
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; } = "";
+        public Email Email { get; set; } = null!;
+        public string Password { get; set; } = "";
+        public UniqueCode UniqueCode { get; set; } = null!;
+        public bool Active { get; set; } = true;
+    }
+}
