5c0869fa9824a5aaa1520a7cebfdffb703249dbc
Gabriel <devzando8182@gmail.com>
2024-06-06 17:46:49 -0300

    adicionando domínio de perfis

---
+++ arbovirose.Domain/Entities/ProfileEntity.cs
@@ -0,0 +1,12 @@
+﻿using arbovirose.Domain.ValueObjects;
+
+namespace arbovirose.Domain.Entities
+{
+    public class ProfileEntity
+    {
+        public ProfileEntity() { }
+        public Guid Id { get; set; }
+        public Office Office { get; set; } = null!;
+        public ICollection<UserEntity> Users { get; set; } = null!;
+    }
+}
---
+++ arbovirose.Domain/ValueObjects/Office.cs
@@ -0,0 +1,28 @@
+﻿using arbovirose.Domain.Enums;
+using arbovirose.Domain.Exceptions;
+
+namespace arbovirose.Domain.ValueObjects
+{
+    public class Office
+    {
+        public string value { get; set; } = "";
+        public Office() { }
+        public Office(string office)
+        {
+            if (!IsValid(office)) throw new InvalidOfficeException();
+            this.value = office;
+        }
+        public bool IsValidOffice(string office)
+        {
+            if (office != TypeOffice.Administrator.ToString() && office != TypeOffice.Editor.ToString()) return false;
+
+            return true;
+        }
+        public bool IsValid(string office)
+        {
+            if (IsValidOffice(office)) return true;
+
+            return false;
+        }
+    }
+}
