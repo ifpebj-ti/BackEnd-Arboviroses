e98c095ed81fbf86fc1b2ad950ef4fb5890c2169
Gabriel <devzando8182@gmail.com>
2024-06-19 20:33:38 -0300

    adicionando domínio de informações da home

---
+++ arbovirose.Domain/ValueObjects/ImgUrl.cs
@@ -0,0 +1,12 @@
+﻿namespace arbovirose.Domain.ValueObjects
+{
+    public class ImgUrl
+    {
+        public string value { get; set; } = "";
+        public ImgUrl() { }
+        public ImgUrl(string url)
+        {
+            
+        }
+    }
+}
---
+++ arbovirose.Infra/Database/Entityframework/Entityconfiguration/InfoHomeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using arbovirose.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace arbovirose.Infra.Database.Entityframework.Entityconfiguration
+{
+    public class InfoHomeConfiguration : IEntityTypeConfiguration<InfoHomeEntity>
+    {
+        public void Configure(EntityTypeBuilder<InfoHomeEntity> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Topic)
+                .HasColumnType("varchar(50)");
+            builder.Property(i => i.Title)
+                .HasColumnType("varchar(50)")
+                .IsRequired();
+            builder.ComplexProperty(i => i.ImgUrl)
+                .IsRequired();
+            builder.Property(i => i.TitleLink)
+                .HasColumnType("varchar(100)");
+            builder.Property(i => i.Link)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+        }
+    }
+}
